55cc66b335147e9db527a25349e18a6b8310848b
Tao Zhao <longtight2000@yahoo.com>
2015-05-08 22:25:22 -0500

    second commit

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 setwd("c:/rwork/exploratory analysis")
-data.Read <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
+data.Read <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings="?")
 data.Sub <- data.Read[data.Read$Date %in% c("1/2/2007", "2/2/2007"),]
 data.Sub$datetime <- strptime(paste(data.Sub$Date, data.Sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 png("plot3.png", width=480, height=480)
